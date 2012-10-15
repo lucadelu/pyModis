e6393277eabd168bb0aa4b1f573b40d68f99a6fd
Luca Delucchi <lucadeluge@gmail.com>
2012-10-15 09:29:08 +0200

    fix exception and some python style rules

--- pymodis/downmodis.py
+++ pymodis/downmodis.py
@@ -27,20 +27,21 @@ import socket
 from ftplib import FTP
 import ftplib
 
+
 class downModis:
   """A class to download MODIS data from NASA FTP repository"""
-  def __init__(self, 
+  def __init__(self,
                 password,
                 destinationFolder,
-                user = "anonymous",
-                url = "e4ftl01.cr.usgs.gov",
-                tiles = None,
-                path = "MOLT/MOD11A1.005",
-                today = None,
-                enddate = None,
-                delta = 10,
-                jpg = False,
-                debug = False
+                user="anonymous",
+                url="e4ftl01.cr.usgs.gov",
+                tiles=None,
+                path="MOLT/MOD11A1.005",
+                today=None,
+                enddate=None,
+                delta=10,
+                jpg=False,
+                debug=False
               ):
     """Initialization function :
 
@@ -81,7 +82,7 @@ class downModis:
     else:
         self.tiles = tiles
     # set destination folder
-    if os.access(destinationFolder,os.W_OK):
+    if os.access(destinationFolder, os.W_OK):
       self.writeFilePath = destinationFolder
     else:
       raise IOError("Folder to store downloaded files does not exist or is not" \
@@ -93,7 +94,7 @@ class downModis:
       self.product = self.path.split('/')[2]
     # write a file with the name of file downloaded
     self.filelist = open(os.path.join(self.writeFilePath, 'listfile' \
-    + self.product + '.txt'),'w')
+    + self.product + '.txt'), 'w')
     # set jpg download
     self.jpeg = jpg
     # today
@@ -109,7 +110,7 @@ class downModis:
     # for logging
     LOG_FILENAME = os.path.join(self.writeFilePath, 'modis' \
     + self.product + '.log')
-    LOGGING_FORMAT='%(asctime)s - %(levelname)s - %(message)s'
+    LOGGING_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
     logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, \
     format=LOGGING_FORMAT)
     self.nconnection = 0
@@ -123,18 +124,18 @@ class downModis:
         fil = os.path.join(self.writeFilePath, f)
         if os.path.getsize(fil) == 0:
             os.remove(fil)
-    
-  def connectFTP(self, ncon = 20):
+
+  def connectFTP(self, ncon=20):
     """ Set connection to ftp server, move to path where data are stored
     and create a list of directories for all days
-    
+
     ncon = number maximum of test to connection at the ftp server
     """
     self.nconnection += 1
     try:
       # connect to ftp server
       self.ftp = FTP(self.url)
-      self.ftp.login(self.user,self.password)
+      self.ftp.login(self.user, self.password)
       # enter in directory
       self.ftp.cwd(self.path)
       self.dirData = []
@@ -155,14 +156,14 @@ class downModis:
     """ Close ftp connection """
     self.ftp.quit()
     self.filelist.close()
-    if self.debug==True:
+    if self.debug == True:
       logging.debug("Close connection %s" % self.url)
 
-  def setDirectoryIn(self,day):
+  def setDirectoryIn(self, day):
     """ Enter in the directory of the day """
     try:
       self.ftp.cwd(day)
-    except (ftplib.error_reply,socket.error), e:
+    except (ftplib.error_reply, socket.error), e:
       logging.error("Error %s entering in directory %s" % e, day)
       self.setDirectoryIn(day)
 
@@ -170,14 +171,17 @@ class downModis:
     """ Come back to old path """
     try:
       self.ftp.cwd('..')
-    except (ftplib.error_reply,socket.error), e:
+    except (ftplib.error_reply, socket.error), e:
       logging.error("Error %s when try to come back" % e)
       self.setDirectoryOver()
 
-  def _str2date(self,strin):
-      """Return a date object from a string"""
+  def _str2date(self, strin):
+      """Return a date object from a string
+
+      string = text string to return date (2012-10-04)
+      """
       todaySplit = strin.split('-')
-      return date(int(todaySplit[0]), int(todaySplit[1]),int(todaySplit[2]))
+      return date(int(todaySplit[0]), int(todaySplit[1]), int(todaySplit[2]))
 
   def _getToday(self):
     """Return the first day for start to download"""
@@ -190,7 +194,7 @@ class downModis:
       # set enday variable to data
     if self.enday != None:
       self.enday = self._str2date(self.enday)
-      
+
   def getListDays(self):
       """ Return a list of all selected days """
       self._getToday()
@@ -199,7 +203,6 @@ class downModis:
       # dirData is reverse sorted
       for i, d in enumerate(self.dirData):
         if d <= today_s:
-          today_avail = d
           today_index = i
           break
       else:
@@ -214,7 +217,7 @@ class downModis:
         delta = 0
         # it make a for cicle from the last value and find the internal delta
         #to remove file outside temporaly range
-        for i in range(-(len(days)),0):
+        for i in range(-(len(days)), 0):
           if days[i] < enday_s:
             break
           else:
@@ -224,13 +227,13 @@ class downModis:
       return days
 
   def getFilesList(self):
-    """ Create a list of files to download, it is possible choose to download 
+    """ Create a list of files to download, it is possible choose to download
     also the jpeg files or only the hdf files"""
-    def cicle_file(jpeg=False,tile=True):
+    def cicle_file(jpeg=False, tile=True):
       finalList = []
       for i in self.listfiles:
         File = i.split('.')
-        # distinguish jpeg files from hdf files by the number of index 
+        # distinguish jpeg files from hdf files by the number of index
         # where find the tile index
         if not tile and not (File.count('jpg') or File.count('BROWSE')):
           finalList.append(i)
@@ -242,7 +245,7 @@ class downModis:
 
     # return the file's list inside the directory of each day
     try:
-      self.listfiles = self.ftp.nlst() 
+      self.listfiles = self.ftp.nlst()
       # download also jpeg
       if self.jpeg:
         # finallist is ugual to all file with jpeg file
@@ -254,21 +257,21 @@ class downModis:
       # not download jpeg
       else:
         if not self.tiles:
-          finalList = cicle_file(tile=False)          
+          finalList = cicle_file(tile=False)
         else:
           finalList = cicle_file()
-      if self.debug==True:
+      if self.debug == True:
         logging.debug("The number of file to download is: %i" % len(finalList))
       return finalList
-    except (ftplib.error_reply,socket.error), e:
+    except (ftplib.error_reply, socket.error), e:
       logging.error("Error %s when try to receive list of files" % e)
       self.getFilesList()
 
   def checkDataExist(self,listNewFile, move = 0):
     """ Check if a file already exists in the directory of download
-    
+
     listNewFile = list of all files, returned by getFilesList function
-    
+
     move = it is useful to know if a function is called from download or move function
     """
     fileInPath = []
@@ -276,7 +279,7 @@ class downModis:
     for f in os.listdir(self.writeFilePath):
       if os.path.isfile(os.path.join(self.writeFilePath, f)):
         fileInPath.append(f)
-    # different return if this method is used from downloadsAllDay() or 
+    # different return if this method is used from downloadsAllDay() or
     # moveFile()
     if move == 0:
       listOfDifferent = list(set(listNewFile) - set(fileInPath))
@@ -284,7 +287,7 @@ class downModis:
       listOfDifferent = list(set(fileInPath) - set(listNewFile))
     return listOfDifferent
 
-  def getNewerVersion(self,oldFile,newFile):
+  def getNewerVersion(self, oldFile, newFile):
     """ Return newer version of a file
     
     oldFile = one of the two similar file
@@ -298,7 +301,7 @@ class downModis:
     else:
       return newFile
 
-  def _downloadFile(self,filDown,filHdf):
+  def _downloadFile(self, filDown, filHdf):
     """Download the single file
     
     filDown = name of the file to download
@@ -312,12 +315,12 @@ class downModis:
       if self.debug==True:
         logging.debug("File %s downloaded" % filDown)
     #if it have an error it try to download again the file
-    except (ftplib.error_reply,socket.error), e:
+    except (ftplib.error_reply, socket.error, ftplib.error_temp), e:
       logging.error("Cannot download %s, retry.." % filDown)
       filSave.close()
       os.remove(filSave.name)
       self.connectFTP()
-      self._downloadFile(filDown,filHdf)
+      self._downloadFile(filDown, filHdf)
     filSave.close()
     orig_size = self.ftp.size(filDown)
     transf_size = os.path.getsize(filSave.name)
@@ -325,13 +328,13 @@ class downModis:
       return 0
     else:
       logging.warning("Different size for file %s - original data: %s, downloaded: %s" %
-                      (filDown, orig_size, transf_size)) 
+                      (filDown, orig_size, transf_size))
       os.remove(filSave.name)
       self._downloadFile(filDown,filHdf)
 
-  def dayDownload(self,listFilesDown):
-    """ Downloads tiles are in files_hdf_consider 
-    
+  def dayDownload(self, listFilesDown):
+    """ Downloads tiles are in files_hdf_consider
+
     listFilesDown = list of the files to download, returned by checkDataExist function
     """
     # for each file in files' list
@@ -346,18 +349,18 @@ class downModis:
           + fileSplit[-1])
           numFiles = len(oldFile)
           if numFiles == 0:
-            file_hdf = os.path.join(self.writeFilePath,i)
+            file_hdf = os.path.join(self.writeFilePath, i)
           elif numFiles == 1:
             # check the version of file  
-            fileDown = self.getNewerVersion(oldFile[0],i)
+            fileDown = self.getNewerVersion(oldFile[0], i)
             if fileDown != oldFile[0]:
-              os.remove(os.path.join(self.writeFilePath,oldFile[0]))
-              file_hdf = os.path.join(self.writeFilePath,fileDown)
+              os.remove(os.path.join(self.writeFilePath, oldFile[0]))
+              file_hdf = os.path.join(self.writeFilePath, fileDown)
           elif numFiles > 1:
             logging.error("There are to much files for %s" % i)
             #raise EOFError("There are to much file with the same prefix")
           if numFiles == 0 or (numFiles == 1 and fileDown != oldFile[0]):
-            self._downloadFile(i,file_hdf)
+            self._downloadFile(i, file_hdf)
 
   def downloadsAllDay(self, clean = False):
     """ Downloads all the tiles considered """
@@ -365,7 +368,7 @@ class downModis:
     if clean:
         self.removeEmptyFiles()
     days = self.getListDays()
-    if self.debug==True:
+    if self.debug == True:
       logging.debug("The number of days to download is: %i" % len(days))
     #for each day
     for day in days:
@@ -379,7 +382,7 @@ class downModis:
       self.dayDownload(listFilesDown)
       self.setDirectoryOver()
     self.closeFTP()
-    if self.debug==True:
+    if self.debug == True:
       logging.debug("Download terminated")
     return 0
 
@@ -418,9 +421,9 @@ class downModis:
     # the lenght of list of days and delta are ugual
     else:
       logger.info("All right!!")
-    
+
   def debugMaps(self):
-    """This function is useful to debug the number of maps to download for 
+    """This function is useful to debug the number of maps to download for
     each day"""
     logger = debugLog()
     days = self.getListDays()
@@ -429,4 +432,4 @@ class downModis:
       listAllFiles = self.getFilesList()
       string = day + ": " + str(len(listAllFiles)) + "\n"
       logger.debug(string)
-      self.setDirectoryOver()   
+      self.setDirectoryOver()
