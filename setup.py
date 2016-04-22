4e6e46fdfef808b7525e9822bb94965575bd85f4
XavierCLL <xavier.corredor.llano@gmail.com>
2016-04-22 17:55:58 -0500

    set utf-8 encoding in setup

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import os
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 
 setup(
